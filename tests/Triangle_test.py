92f5f8e3fcabeff1fb6834fa70bf0f482412c690
Андрей <ad6803884@yandex.ru>
2025-05-16 22:56:06 +0300

    Добавлены тесты метода проверки является ли треугольник прямоугольным

--- tests/Triangle_test.py
+++ tests/Triangle_test.py
@@ -44,6 +44,18 @@ class TestCircle(unittest.TestCase):
         expected_perimeter = 27.313708
         self.assertAlmostEqual(triangle.perimeter(), expected_perimeter, places=5)
 
+    def test_is_right_triangle_5_5_5(self):
+        triangle = Triangle(5, 5, 5)
+        self.assertFalse(triangle.is_right_triangle())
+
+    def test_is_right_triangle_8_8_11313708(self):
+        triangle = Triangle(8, 8, 11.313708)
+        self.assertTrue(triangle.is_right_triangle())
+
+    def test_is_right_triangle_135_1250_1250(self):
+        triangle = Triangle(135, 1250, 1250)
+        self.assertFalse(triangle.is_right_triangle())
+
 
 if __name__ == '__main__':
     unittest.main()

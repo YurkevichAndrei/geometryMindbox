327c6c306da186344cf4fa76313cf2e86af57174
Андрей <ad6803884@yandex.ru>
2025-05-16 16:24:28 +0300

    Initial commit

---
+++ shapes/Circle.py
@@ -0,0 +1,11 @@
+import math
+
+import Shape
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius ** 2
---
+++ shapes/Shape.py
@@ -0,0 +1,7 @@
+from abc import abstractmethod, ABC
+
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass

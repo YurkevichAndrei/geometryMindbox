9d5e1916825f17b2c41c457a17c7e7a2a8f36d71
Андрей <ad6803884@yandex.ru>
2025-05-16 22:31:06 +0300

    Добавлен метод проверки является ли треугольник прямоугольным

--- shapes/Triangle.py
+++ shapes/Triangle.py
@@ -17,3 +17,9 @@ class Triangle(Shape):
     def area(self):
         poluperimeter = self.perimeter() / 2
         return math.sqrt(poluperimeter * (poluperimeter - self.side_a) * (poluperimeter - self.side_b) * (poluperimeter - self.side_c))
+
+    # метод проверки является ли треугольник прямоугольным
+    def is_right_triangle(self):
+        sides = [self.side_a, self.side_b, self.side_c]
+        sides.sort()
+        return sides[2] ** 2 == sides[0] ** 2 + sides[1] ** 2

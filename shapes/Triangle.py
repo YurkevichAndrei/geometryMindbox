49c2a6880e8c2a9258ed01da953428506d48c956
Андрей <ad6803884@yandex.ru>
2025-05-16 16:44:45 +0300

    Добавлены методы расчета периметра и площади

--- shapes/Triangle.py
+++ shapes/Triangle.py
@@ -9,5 +9,11 @@ class Triangle(Shape):
         self.side_b = side_b
         self.side_c = side_c
 
+    # метод расчета переметра
+    def perimeter(self):
+        return (self.side_a + self.side_b + self.side_c) / 2
+
+    # метод расчета площади по формуле Герона (по трем сторонам)
     def area(self):
-        pass
+        perimeter = self.perimeter()
+        return math.sqrt(perimeter * (perimeter - self.side_a) * (perimeter - self.side_b) * (perimeter - self.side_c))
